c2d028adc383b88c6216c5a9cfc5e80afa28654b
Dinuki Prarthana <139613301+Dinuki85@users.noreply.github.com>
2024-12-23 14:19:51 +0000

    Feat:ContactUS Page updated

--- MyMobileApp/components/ContactUs.js
+++ MyMobileApp/components/ContactUs.js
@@ -1,28 +1,45 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { Divider  , Text} from 'react-native-paper';
 
-export default function ContactUS(){
-    return(
-        <>
-        
-        
-        </>
+export default function ContactUs() {
+  return (
+    <>
+      <View style={styles.header}>
+        <Text variant="headlineLarge">Contact US</Text>
+        <Divider />
 
-    );
+      </View>
+      <View style={styles.body}>
+
+
+
+      </View>
+      <View style={styles.footer}>
+        <Text variant="headlineLarge">MyApp © 2024</Text>
+        <Divider />
+
+      </View>
+
+    </>
+
+  );
 }
 
 const styles = StyleSheet.create({
   header: {
-    flex:3,
-    width:"100%",
-    
+    flex: 3,
+    width: "100%",
+    alignItems: 'center'
+
   },
-  body:{
-    flex:5,
-    width:"100%",
+  body: {
+    flex: 5,
+    width: "100%"
   },
-  footer:{
-    flex:2,
-    width:"100%",
+  footer: {
+    flex: 2,
+    width: "100%",
+    alignItems: 'center'
 
   }
 });
